ff9db5d1ae3f3db14c2b350a3298b5d3a2e2d14b
Yuri <75639439+yuridmayerpro@users.noreply.github.com>
2024-08-28 19:42:31 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import gdown
 import pandas as pd
+from calcula_macros import calcula_tmb, calcula_metas_macronutrientes1, calcula_metas_macronutrientes2, calcula_metas_macronutrientes3
 
 # Function to load DataFrame from Google Drive
 @st.cache_data
@@ -28,5 +29,25 @@ if drive_url:
     except Exception as e:
         st.error(f"Erro ao carregar a tabela: {e}")
 
+# Input fields
+peso = st.number_input("Peso (kg)", min_value=1, value=80, step=1)
+idade = st.number_input("Idade", min_value=0, value=30, step=1)
+sexo = st.selectbox("Sexo", options=['m', 'f'])
+objetivo = st.selectbox("Objetivo", options=['hipertrofia'])
+
+# Botão para calcular os macronutrientes
+if st.button("Calcular Macros"):
+    # Calcula os macros
+    calorias_alvo1, gramas_proteina1, gramas_carboidrato1, gramas_gordura1 = calcula_metas_macronutrientes1(peso, idade, sexo, objetivo)
+    calorias_alvo2, gramas_proteina2, gramas_carboidrato2, gramas_gordura2 = calcula_metas_macronutrientes2(peso, idade, objetivo)
+    calorias_alvo3, gramas_proteina3, gramas_carboidrato3, gramas_gordura3 = calcula_metas_macronutrientes3(peso, idade, sexo, objetivo)
+
+    calorias_alvo = int(round(np.average([calorias_alvo1, calorias_alvo2, calorias_alvo3]), 0))
+    proteina_alvo = int(round(np.average([gramas_proteina1, gramas_proteina2, gramas_proteina3]), 0))
+    carboidrato_alvo = int(round(np.average([gramas_carboidrato1, gramas_carboidrato2, gramas_carboidrato3]), 0))
+    
+    st.write(f"Calorias alvo: {calorias_alvo} kcal")
+    st.write(f"Proteínas alvo: {proteina_alvo} g")
+    st.write(f"Carboidratos alvo: {carboidrato_alvo} g")
 
 

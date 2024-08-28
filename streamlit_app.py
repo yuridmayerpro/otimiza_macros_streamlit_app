f3f5a46ad7f3056e4983ccc97959fc23a592f73e
Yuri <75639439+yuridmayerpro@users.noreply.github.com>
2024-08-28 19:21:54 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,11 @@ st.title("Calculadora de Macronutrientes")
 # URL to the Google Drive file containing the df_taco dataset
 drive_url = st.text_input("URL do arquivo no Google Drive")
 
+# Extrair o FILE_ID e criar a URL de download
+file_id = drive_url.split("/d/")[1].split("/edit")[0]
+download_url = f"https://drive.google.com/uc?id={file_id}"
+
 if drive_url:
-    df_taco = load_data_from_drive(drive_url)
+    df_taco = load_data_from_drive(download_url)
     st.write("Exemplo de dados carregados:")
-    #st.write(df_taco.head())
     st.dataframe(df_taco)

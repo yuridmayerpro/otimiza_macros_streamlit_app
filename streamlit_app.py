b6593aac4e579be00126caf85fc2ffe22015fdd3
Yuri <75639439+yuridmayerpro@users.noreply.github.com>
2024-08-28 18:55:15 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
-st.write('Hello world!')
+# Function to load DataFrame from Google Drive
+@st.cache_data
+def load_data_from_drive(url):
+    output = 'df_taco.xlsx'
+    gdown.download(url, output, quiet=False)
+    df = pd.read_excel(output)
+    return df
+
+st.title("Calculadora de Macronutrientes")
+
+# URL to the Google Drive file containing the df_taco dataset
+drive_url = st.text_input("URL do arquivo no Google Drive")
+
+if drive_url:
+    df_taco = load_data_from_drive(drive_url)
+    st.write("Exemplo de dados carregados:")
+    #st.write(df_taco.head())
+    st.dataframe(df_taco)

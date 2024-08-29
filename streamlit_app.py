e6e1ca896dc3b267c986fd5b9cb0f84e522915f1
Yuri <75639439+yuridmayerpro@users.noreply.github.com>
2024-08-28 22:24:39 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import gdown
 import pandas as pd
 import numpy as np
 from calcula_macros import calcula_tmb, calcula_metas_macronutrientes1, calcula_metas_macronutrientes2, calcula_metas_macronutrientes3
+from otimizador import objective, early_stopping_callback, otimiza
 
 
 
